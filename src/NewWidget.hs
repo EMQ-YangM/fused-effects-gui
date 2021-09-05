d9fcfa1d35e7ac221daf819d423be9b1ba9a2035
EMQ-YangM <yangm@emqx.io>
2021-09-05 18:54:30 +0800

    ste 1

--- src/NewWidget.hs
+++ src/NewWidget.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeApplications #-}
@@ -14,6 +16,7 @@ import Control.Monad
 import Control.Monad.IO.Class
 import Data.Dynamic
 import Data.Foldable (forM_)
+import Data.Functor (Functor)
 import Data.IORef
 import Data.Kind
 import Data.Text (Text, pack)
@@ -46,16 +49,15 @@ data UIState = UIState
 
 type UI sig m = Has (Reader UIEnv :+: State UIState) sig m
 
-data Widget model = forall parent a.
+data Widget model = forall a.
   Widget
   { rect :: Rectangle Int,
     model :: (IORef a, Lens' a model),
-    children :: [SomeWidget],
-    parent :: Widget parent
+    children :: [SomeWidget]
   }
 
 instance Show model => Show (Widget model) where
-  show (Widget rec (ref, m) children _) =
+  show (Widget rec (ref, m) children) =
     show rec ++ " \n"
       ++ unsafePerformIO
         ( do
@@ -91,8 +93,7 @@ widgetCreate ref len =
     Widget
       { rect = Rectangle 30 40,
         model = (ref, len),
-        children = [],
-        parent = undefined
+        children = []
       }
 
 t :: IO ()
@@ -105,12 +106,10 @@ t = do
             children =
               [ widgetCreate refA va,
                 widgetCreate refA vb
-              ],
-            parent = undefined
+              ]
           }
   --   nw = w {parent = undefined}
   print w
-
 
 data T a = T
   { val :: a,
@@ -123,3 +122,67 @@ data C a = C
     cv :: T a,
     pr :: [T a]
   }
+
+data Zipper a = Zipper
+  { _zval :: a,
+    _left :: [Zipper a],
+    _center :: Zipper a,
+    _right :: [Zipper a]
+  }
+
+makeLenses ''Zipper
+
+-- instance Show a => Show (Zipper a) where
+--   show (Zipper ls c rs) = show ls ++ " [" ++ show c ++ "] " ++ show rs
+
+-- leftMove :: Zipper a -> Zipper a
+-- leftMove (Zipper (l : ls) c rs) = Zipper ls l (c : rs)
+-- leftMove a = a
+
+-- rightMove :: Zipper a -> Zipper a
+-- rightMove (Zipper ls c (r : rs)) = Zipper (c : ls) r rs
+-- rightMove a = a
+
+-- >>> tz
+-- >>> leftMove tz
+-- >>> rightMove $ rightMove tz
+-- [3,2,1] [4] [5,6,7]
+-- [2,1] [3] [4,5,6,7]
+-- [5,4,3,2,1] [6] [7]
+-- tz :: Zipper Int
+-- tz = Zipper [3, 2, 1] 4 [5, 6, 7]
+
+-- type ZZ a = Zipper (Zipper a)
+
+-- >>> tzz
+-- >>> tzz & center %~ leftMove
+-- >>> leftMove  tzz
+-- [[3,2,1] [4] [5,6,7]] [[3,2,1] [4] [5,6,7]] [[3,2,1] [4] [5,6,7]]
+-- [[3,2,1] [4] [5,6,7]] [[2,1] [3] [4,5,6,7]] [[3,2,1] [4] [5,6,7]]
+-- [] [[3,2,1] [4] [5,6,7]] [[3,2,1] [4] [5,6,7],[3,2,1] [4] [5,6,7]]
+-- tzz :: ZZ Int
+-- tzz = Zipper [tz] tz [tz]
+
+-- tk = tzz & center %~ leftMove
+
+-- data Fix f a
+--   = Fix (f (Fix f a))
+--   | End a
+--   deriving (Functor)
+
+-- instance (Foldable f, Show a) => Show (Fix f a) where
+--   show (End a) = "End " ++ show a
+--   show (Fix v) = "Fix " ++ concatMap show v
+
+-- type ZF a = Fix Zipper a
+
+-- -- >>> tzf
+-- -- Fix Fix End 0End 10End 100End 1000End 1End 2Fix End 3
+-- tzf :: ZF Int
+-- tzf =
+--   Fix
+--     ( Zipper
+--         [Fix (Zipper [End 0, End 10, End 100, End 1000] (End 1) [])]
+--         (End 2)
+--         [Fix (Zipper [] (End 3) [])]
+--     )
